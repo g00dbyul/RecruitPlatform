c724cc4f72188606b35a274a0f0191a86ab5f883
g00dbyul <skypower4203@gmail.com>
2026-04-18 21:14:03 +0900

    feat: implement recruit repository adapter

--- src/main/java/com/godbyul/recruit_platform/domain/recruit/repository/RecruitRepository.java
+++ src/main/java/com/godbyul/recruit_platform/domain/recruit/repository/RecruitRepository.java
@@ -1,8 +1,18 @@
 package com.godbyul.recruit_platform.domain.recruit.repository;
 
 import com.godbyul.recruit_platform.domain.recruit.command.CreateRecruitCommand;
+import com.godbyul.recruit_platform.domain.recruit.query.FindRecruitQuery;
 import com.godbyul.recruit_platform.entity.Recruit;
+
+import java.util.List;
+import java.util.UUID;
 
 public interface RecruitRepository {
     Recruit createRecruit(CreateRecruitCommand command);
+
+    List<Recruit> findRecruit(FindRecruitQuery query);
+
+    Recruit findRecruitById(UUID id);
+
+    void deleteRecruitById(UUID id);
 }
